418fe9cf2c39274058f454f446749b81136a7ebb
Tatsu Nana <tatsunana@Tatsus-Air.lan>
2024-09-09 08:11:35 +1000

    Creating a post page

--- src/app/create-post/[userId]/page.tsx
+++ src/app/create-post/[userId]/page.tsx
@@ -3,6 +3,9 @@ import { getServerAuthSession } from "~/server/auth";
 import { redirect } from "next/navigation";
 import { HydrateClient } from "~/trpc/server";
 import PostMain from "~/app/components/header/PostHeader";
+import { Dialog, DialogBackdrop, DialogPanel, DialogTitle } from '@headlessui/react';
+import { ExclamationTriangleIcon } from '@heroicons/react/24/outline';
+import Main from "../Main";
 
 export default async function Post({ params }: { params: { userId: string } }) {
     const session = await getServerAuthSession();
@@ -16,8 +19,10 @@ export default async function Post({ params }: { params: { userId: string } }) {
 
     return (
         <HydrateClient>
-            <div className="flex flex-col items-center">
-                <PostMain userData={session.user} />
+            <div className="flex-auto text-[18px] box-border">
+                <div>
+                    <Main userData={session?.user} />
+                </div>
             </div>
         </HydrateClient>
     )
