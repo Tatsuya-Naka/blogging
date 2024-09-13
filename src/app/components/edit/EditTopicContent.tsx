f20048e0797a671af9334e4b54b61d959cb1a10b
Tatsu Nana <tatsunana@Tatsus-Air.lan>
2024-09-14 09:10:51 +1000

    fix await

--- src/app/components/edit/EditTopicContent.tsx
+++ src/app/components/edit/EditTopicContent.tsx
@@ -83,7 +83,7 @@ export default function EditTopicContent({ userData }: Props) {
             });
             console.log("Updated Topic: ", result);
             setIsUpdated(true);
-            refetchTopic();
+            await refetchTopic();
         } catch (err) {
             console.log("Error occured during updating topic: ", err);
         }
